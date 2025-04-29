319d1d53b0a6d9af2f931958facba3852cada53f
Johng117 <johngorman117@gmail.com>
2025-04-29 18:10:16 +0100

    added more sections of form

---
+++ index.js
@@ -0,0 +1,11 @@
+const dragEl = document.getElementById("p1");
+
+function dragstartHandler(ev) {
+    ev.stopPropagation();
+  // Add the target element's id to the data transfer object
+  ev.dataTransfer.setData("text/plain", ev.target.id);
+  console.log("dragging started");
+}
+
+// Add the ondragstart event listener
+dragEl.addEventListener("dragstart", dragstartHandler);

c1e4fde778574f843054880c39f9c43afca055e3
Johng117 <johngorman117@gmail.com>
2025-04-30 22:06:49 +0100

    added file access logic and updates to index.html

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-const dragEl = document.getElementById("p1");
+const fileUploadElement = document.getElementById("droppable-element");
+const uploadInfoElement = document.getElementById("droppable-info-text");
+const uploadInfoImage = document.getElementById("droppable-info-img");
+const fileTestRegex = new RegExp("^S+(.jpg|.png)", "g");
 
-function dragstartHandler(ev) {
-    ev.stopPropagation();
-  // Add the target element's id to the data transfer object
-  ev.dataTransfer.setData("text/plain", ev.target.id);
-  console.log("dragging started");
+const pickerOptions = {
+  types: [
+    {
+      description: "Images",
+      accept: {
+        "image/*": [".png", ".gif", ".jpeg", ".jpg"],
+      },
+    },
+  ],
+  excludeAcceptAllOption: true,
+  multiple: false,
+};
+
+let fileHandle;
+
+async function getFile() {
+  [fileHandle] = await window.showOpenFilePicker(pickerOptions);
+
+  console.log(fileHandle);
+  let validFile = fileTestRegex.test(fileHandle.name);
+  if (!validFile) {
+    uploadInfoElement.style.color = "red";
+    uploadInfoImage.setAttribute("style", "stroke:red");
+    uploadInfoElement.innerText = "Error: Image is not JPG or PNG";
+  }
 }
 
-// Add the ondragstart event listener
-dragEl.addEventListener("dragstart",dragstartHandler);
+fileUploadElement.addEventListener("click", getFile);
